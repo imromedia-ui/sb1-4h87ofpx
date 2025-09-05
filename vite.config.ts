8d57e823614fb8893a0c8737b0f5101f63e2e81c
imromedia-ui <imro.media@gmail.com>
2025-09-04 23:02:21 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        policies: resolve(__dirname, 'policies.html')
+      },
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
